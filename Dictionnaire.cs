a041d55910ff54a68bf1cfd7549142c9a67cc6a5
floshv1 <cazac.florian@gmail.com>
2023-11-17 17:51:59 +0100

    Add Dictionnaire class and implement ReadFile and toString methods

--- Dictionnaire.cs
+++ Dictionnaire.cs
@@ -4,26 +4,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace dictionnaire
+namespace projet_algo
 {
     public class Dictionnaire
     {
         string langue;
-        list<string[]> dico;
+        List<string[]> dico;
 
-        public Dictionnaire(string fileName)
+        public Dictionnaire(string langue)
         {
-            
+            this.langue = langue;
+            string fileName = "Mots_" + langue + ".txt";
+            ReadFile(fileName);
         }
 
-        public list<string[]> ReadFile(string fileName)
+        public void ReadFile(string fileName)
         {
+            dico = new List<string[]>();
             StreamReader sr = new StreamReader(fileName);
             string line = sr.ReadLine();
 
+            while (line != null)
+            {
+                string[] words = line.Split(" ");
+                dico.Add(words);
+                line = sr.ReadLine();
+            }
+            sr.Close();
+        }
 
+        
 
-
+        public string toString()
+        {
+            int nombreDeMots = 0;
+            foreach (string[] words in dico)
+            {
+                nombreDeMots += words.Length;
+            }
+            string strNombreDeMots= "Il y a " + nombreDeMots + " mots dans le dictionnaire.";
+            return strNombreDeMots + "\n" + "La langue du dictionnaire est " + langue + ".";
         }
 
     }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace projet_algo // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionnaire dico = new Dictionnaire("Français");
+            Console.WriteLine(dico.toString());       
+        }
+    }
+}
